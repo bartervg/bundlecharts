65c4b00f0f9cca4e8fa6e628a41d2821ca5b4261
bartervg <bartervg@gmail.com>
2021-07-09 11:05:13 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>🍒 Bundle Charts (🌸◠‿◠)</title>
+  <meta name="description" content="The definitive bundle charts site undistracted by library management features" />
+	
+  <link rel="preconnect" href="https://cdn.bundlecharts.com" />
+  <link rel="preconnect" href="https://steamcdn-a.akamaihd.net/" />
+  <link rel="preconnect" href="https://cdn.akamai.steamstatic.com/" />  
+  <link rel="icon" href="/favicon.ico" />
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <link rel="stylesheet" href="https://cdn.bundlecharts.com/css/style.210709.css" />
+</head>
+
+<body>
+  <h1>List of Bundles</h1>
+  <table>
+    <thead></thead>
+    <tbody>
+<?php
+  // connect to database
+      
+  // select bundle info
+  
+  // display bundles
+  print '<tr><td>bundle name</td><td>...</td></tr>';
+?>
+  </tbody>
+  <tfoot></tfoot>
+  </table>
+  <script src="https://cdn.bundlecharts.com/css/js/script.210709.js"></script>
+</body>
+</html>
